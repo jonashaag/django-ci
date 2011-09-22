53a41ac6de213ec919d2687621448d83e44aae04
Jonas Haag <jonas@lophus.org>
2011-09-22 15:45:21 +0200

    commit_link.html -> commit_link.inc.html

--- ci/templatetags/ci.py
+++ ci/templatetags/ci.py
@@ -2,6 +2,6 @@ from django.template import Library
 
 register = Library()
 
-@register.inclusion_tag('ci/commit_link.html')
+@register.inclusion_tag('ci/commit_link.inc.html')
 def commit_link(commit):
     return {'commit': commit}
