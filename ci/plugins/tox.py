649e9d14c5e3d4b529ca8d6a41aa9b047b317bcc
Jonas Haag <jonas@lophus.org>
2011-09-28 14:46:52 +0200

    Removed tox and unittest2 plugins

--- ci/plugins/tox.py
+++
@@ -1,8 +0,0 @@
-from ci.plugins.base import Plugin, CommandBasedBuilder
-
-class ToxBuilder(CommandBasedBuilder):
-    cmd = ['tox']
-
-class ToxPlugin(Plugin):
-    def get_builders(self):
-        return {'tox': ToxBuilder}
